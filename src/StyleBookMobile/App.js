47ae39c5e6da14e8c3905c5e264e2703b9c81f55
Quang Lê <quanggr8@gmail.com>
2022-01-05 15:06:15 +0700

    fix app.js

--- src/StyleBookMobile/App.js
+++ src/StyleBookMobile/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import * as Font from "expo-font"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Provider } from "react-redux"
+import { store, persistor } from "./redux/store"
+import { PersistGate } from "redux-persist/integration/react"
+import NavigationScreen from "./screens/navigation"
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      resourceLoaded: false,
+    }
+  }
+
+  async LoadResource() {
+    await Font.loadAsync({
+      "SF Pro Display Bold": {
+        uri: require("./assets/fonts/SF-Pro-Display-Bold.ttf"),
+        display: Font.FontDisplay.FALLBACK,
+      },
+      "SF Pro Display Heavy": {
+        uri: require("./assets/fonts/SF-Pro-Display-Heavy.ttf"),
+        display: Font.FontDisplay.FALLBACK,
+      },
+      "SF Pro Display Regular": {
+        uri: require("./assets/fonts/SF-Pro-Display-Regular.ttf"),
+        display: Font.FontDisplay.FALLBACK,
+      },
+      "SF Pro Display Semibold": {
+        uri: require("./assets/fonts/SF-Pro-Display-Semibold.ttf"),
+        display: Font.FontDisplay.FALLBACK,
+      },
+    })
+    this.setState({ resourceLoaded: true })
+  }
+
+  componentDidMount() {
+    this.LoadResource()
+  }
+  render() {
+    if (this.state.resourceLoaded)
+      return (
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <NavigationScreen />
+          </PersistGate>
+        </Provider>
+      )
+    else return null
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
